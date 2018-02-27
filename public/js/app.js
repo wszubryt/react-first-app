fe5a5e70a8f2a78f7be71b2bc6b7f1fc17eca299
wojciech.szubryt <wojciech.szubryt@avra.pl>
2018-02-27 14:51:19 +0100

    render product

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,33 @@
+class Product extends React.Component {
+    render() {
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src='images/products/pizza-margherita.jpg' />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='description'>
+                        <a>Pizza margherita</a>
+                        <p>Just standard pizza margherita.</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img
+                            className='ui avatar image'
+                            src='images/avatars/avatar.png'
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 class ProductList extends React.Component {
     render() {
         return (
             <div className='ui unstackable items'>
-                Hello, friend! I am a basic React component.
+                <Product />
             </div>
         );
     }
