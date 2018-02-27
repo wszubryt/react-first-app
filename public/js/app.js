eedd658bbb113bb355782ee5106bf995872fbc6b
wojciech.szubryt <wojciech.szubryt@avra.pl>
2018-02-27 16:11:42 +0100

    use data object, use props in components

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,28 @@ class Product extends React.Component {
         return (
             <div className='item'>
                 <div className='image'>
-                    <img src='images/products/pizza-margherita.jpg' />
+                    <img src={this.props.productImageUrl} />
                 </div>
                 <div className='middle aligned content'>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon' />
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className='description'>
-                        <a>Pizza margherita</a>
-                        <p>Just standard pizza margherita.</p>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
                     </div>
                     <div className='extra'>
                         <span>Submitted by:</span>
                         <img
                             className='ui avatar image'
-                            src='images/avatars/avatar.png'
+                            src={this.props.submitterAvatarUrl}
                         />
                     </div>
                 </div>
@@ -25,9 +35,19 @@ class Product extends React.Component {
 
 class ProductList extends React.Component {
     render() {
+        const product = window.Data.products[0];
+
         return (
             <div className='ui unstackable items'>
-                <Product />
+                <Product
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitterAvatarUrl={product.submitterAvatarUrl}
+                    productImageUrl={product.productImageUrl}
+                />
             </div>
         );
     }
---
+++ public/js/data.js
@@ -0,0 +1,13 @@
+window.Data = {
+    products: [
+        {
+            id: 1,
+            title: 'Pizza margherita',
+            description: 'Just pizza margherita.',
+            url: '#',
+            votes: 5,
+            submitterAvatarUrl: 'images/avatars/avatar.png',
+            productImageUrl: 'images/products/pizza-margherita.jpg',
+        },
+    ]
+};
