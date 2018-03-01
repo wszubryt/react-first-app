714eb44eb7ddaba41268b9c0402674f31520e596
wojciech.szubryt <wojciech.szubryt@avra.pl>
2018-03-01 09:17:58 +0100

    remove everything from app (init)

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,1 @@
-class ProductList extends React.Component {
-    render() {
-        return (
-            <div className='ui unstackable items'>
-                Hello, friend! I am a basic React component.
-            </div>
-        );
-    }
-}
+// TODO
